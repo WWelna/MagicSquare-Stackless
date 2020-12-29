7aea02b9af7b29376c1c91e2beb9aa8b595fb1ba
WWelna <WWelna@OccultusTerra.com>
2020-12-29 01:52:45 -0600

    Update magicsquare.py
    lol typo

--- magicsquare.py
+++ magicsquare.py
@@ -1,4 +1,4 @@
-  # Copyright (C) 2020 William Welna (wwelna@occultusterra.com)
+# Copyright (C) 2020 William Welna (wwelna@occultusterra.com)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
